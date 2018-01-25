645360f047caa08b7f7948dd9c9fff0ff3fa9431
vradu <vradu@pentalog.com>
2018-01-25 13:59:07 +0200

    changed ips

--- noblesse-oblige/src/app/collaboration/collaboration.constants.ts
+++ noblesse-oblige/src/app/collaboration/collaboration.constants.ts
@@ -1,4 +1,4 @@
 export class CollaborationConstants {
-	public static BASE_SOCKET_URL :string = 'http://localhost:8081/';
+	public static BASE_SOCKET_URL :string = 'http://34.244.36.79:8081/';
 	public static GLOBAL_ROOM : string = 'root';
 }
