ed895a58ee4334e76c647e6565fe419c9a392f3b
vradu <vradu@pentalog.com>
2018-01-25 13:42:44 +0200

    changed ips

--- noblesse-oblige/src/app/generics/generics.constants.ts
+++ noblesse-oblige/src/app/generics/generics.constants.ts
@@ -1,6 +1,6 @@
 export class GenericConstants {
 	//public static BASE_URL :string = 'http://178.62.127.9:8000';
-	public static BASE_URL :string = 'http://localhost:8080';
-	public static BASE_SOCKET_URL :string = 'http://locahost:8081';
-	public static BASE_FILE_URL :string = 'http://localhost:9000/';
+	public static BASE_URL :string = 'http://34.244.36.79:8080';
+	public static BASE_SOCKET_URL :string = 'http://34.244.36.79:8081';
+	public static BASE_FILE_URL :string = 'http://34.244.36.79:9000/';
 }
